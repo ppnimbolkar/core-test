bd6643517554a43c7de9232c8c5b86e20e6374cc
antoniomerlin <gauravsinghbr@hotmail.com>
2014-08-27 17:46:54 +0530

    threaded server test

---
+++ networking/src/networking/ServerThreaded.java
@@ -0,0 +1,91 @@
+package networking;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+* This is a multithreaded server that echo all client input.
+*@version 1.15
+*/
+
+public class ServerThreaded
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			int i = 1;
+			ServerSocket s = new ServerSocket(555);
+			
+			while(true)
+			{
+				Socket incoming = s.accept();
+				System.out.println("Spawning " + i);
+				Runnable r = new ThreadedEchoHandler(incoming);
+				Thread t = new Thread(r);
+				t.start();
+				i++;
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+/**
+*This class handles the client input for one server socket connection.
+*/
+
+class ThreadedEchoHandler implements Runnable
+{
+	/**
+	 * Construct a handler
+	 * @param i the incoming socket
+	 * @param c the counter for the handlers (used in prompts)
+	 */
+	
+	public ThreadedEchoHandler(Socket i)
+	{
+		incoming = i;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			try
+			{
+				InputStream inStream = incoming.getInputStream();
+				OutputStream outStream = incoming.getOutputStream();
+				
+				Scanner in = new Scanner(inStream);
+				PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+				
+				out.println("Hey There! Enter BYE to EXIT.");
+				
+				//echo client input
+				boolean done = false;
+				while(!done && in.hasNextLine())
+				{
+					String line = in.nextLine();
+					out.println("Echo: " + line);
+					if(line.trim().equals("BYE"))
+						done = true;
+				}
+			}
+			finally
+			{
+				incoming.close();
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private Socket incoming;
+}
